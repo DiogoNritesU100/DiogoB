4cd2a8b7fa3cab0390917bb7dd5292190e6fb965
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-05 11:15:13 +0000

    Add docs to Argumentos project

---
+++ Argumentos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Argumentos
+{
+  /// <summary>
+  /// This is a really cool program!
+  /// </summary>
+
+  class Program
+  {
+    /// <summary>
+    /// Program starts here.
+    /// </summary>
+    /// <param name="args">The command-line arguments.</param>
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < args.Length; i++)
+      {
+        // This will write argument in console
+        Console.WriteLine(args[i]);
+      }
+    }
+  }
+}
