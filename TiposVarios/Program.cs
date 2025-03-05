07c1fed4fe2e4e105203a428f5655a2b77b5a977
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-05 12:21:05 +0000

    Add TiposVarios v1

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 5;
+uint b = 5u;
+long c = 5l;
+byte d = 3;
+
+Console.Write(a);
+Console.Write(b);
+Console.Write(c);
+Console.Write(d);
