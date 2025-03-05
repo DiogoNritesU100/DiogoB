1ff45cb82bf231867a1a3c34bf16ac834135fe9a
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-05 12:44:33 +0000

    Add characters

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,10 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = 5;
-uint b = 5u;
-long c = 5l;
-byte d = 3;
+using System;
+using System.Text;
 
-Console.Write(a);
-Console.Write(b);
-Console.Write(c);
-Console.Write(d);
+namespace TiposVarios;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int a = 5;
+            uint b = 5u;
+            long c = 5;
+            byte d = 3;
+
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+
+            char copySym = '\u00A9';
+            char weird = '\u07D0';
+
+            Console.WriteLine(copySym);
+            Console.WriteLine(weird);
+            
+        }
+    }
+}
