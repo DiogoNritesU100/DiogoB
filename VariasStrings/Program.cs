bf6a95d008795326e0760f75d3a693dfd3f4e1d3
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-07 16:21:31 +0000

    Add more strings VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -4,6 +4,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        int x = 123;
+        int y = -123;
+
         Console.WriteLine("Hello, World!");
         Console.WriteLine(@"\nHello, World!\n");
         Console.WriteLine("\u07F7\u08F2");
@@ -11,6 +14,17 @@ internal class Program
         Console.WriteLine("\" This is in double quotes\"");
         Console.WriteLine(@"""This is in double quotes""");
         
+        Console.WriteLine("a" + 2);
+        Console.WriteLine("abc" + x);
+        
+        string myStr = $"valor de x e {x}";
+        Console.WriteLine(myStr);
+        myStr  = $"{x} mais {y} e igual a {x + y}";
+        Console.WriteLine(myStr);
+
+        myStr = string.Format("Y é {1}, X é {0}", x , y);
+        Console.WriteLine(myStr);
+        Console.WriteLine("Y é {1}, X é {0}", x , y);
 
     }
 }
