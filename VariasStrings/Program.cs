45cf937f3126ed6632f64769d4724aad954edb82
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-07 14:28:14 +0000

    Add VariasSatrings project

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(@"\nHello, World!\n");
+        Console.WriteLine("\u07F7\u08F2");
+        Console.WriteLine(@"\u07F7\u08F2");
+        Console.WriteLine("\" This is in double quotes\"");
+        Console.WriteLine(@"""This is in double quotes""");
+        
+
+    }
+}
